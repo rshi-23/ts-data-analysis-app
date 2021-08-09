0b7d543c458b5c3401839c38ffea7a925ddd2eb5
raymon-shi <61591942+raymon-shi@users.noreply.github.com>
2021-08-09 15:32:17 -0400

    added docstrings for functions

--- setup_functions.py
+++ setup_functions.py
@@ -3,24 +3,39 @@ import numpy as np
 import pandas as pd
 
 
-# a function that converts the column index range and returns a list of column header names
 def get_header_names(raw_data_headers, header_index_range):
-    header_names_list = []
+    """
+    This function takes in the raw data column headers and header index list and creates a list with the header names
+
+    :param raw_data_headers: A list of all the column names from raw data
+    :param header_index_range: A list of indices for a specific parameter
+    :return: A list of header names that correspond to the header_index_range list
+    """
+
+    header_names_list = list()
     for index in header_index_range:
         header_names_list.append(raw_data_headers[index])
 
     return header_names_list
 
 
-# a function that converts a specific range of the csv into numerics (floats)
 def convert_to_int(header_index_range, raw_data_headers, dataframe):
+    """
+    This function converts a specific range of the dataframe into a numeric type dataframe.
+
+    :param header_index_range: A list of indices for specific parameters
+    :param raw_data_headers: A list of all the column names from raw data
+    :param dataframe: A dataframe with columns that need to be converted from strings to numerics
+    :return: A dataframe with a specific section converted from strings to numerics
+    """
+
     # replace NaNs and - (no values/blank spaces) with a very large placeholder number
     dataframe.replace(np.nan, '999999999', regex=True, inplace=True)
     dataframe.replace('-', '999999999', regex=True, inplace=True)
 
     header_names_range = get_header_names(raw_data_headers, header_index_range)
 
-    # turn this header range from strings to ints
+    # turn this header range from strings to numerics
     dataframe[header_names_range] = dataframe[header_names_range].apply(pd.to_numeric)
 
     # replace that very large placeholder number with NaNs again
@@ -30,9 +45,16 @@ def convert_to_int(header_index_range, raw_data_headers, dataframe):
     return dataframe
 
 
-# a function that gets the indice range for specific headers
-def index_range(keyword, header_list, dataframe):
-    index_list = []
+def index_range(keyword, header_list):
+    """
+    This function takes in a keyword and a list of column header names and creates a list of indices that contains the
+    specified keyword.
+    :param keyword: A header or part of a header that you are interested in.
+    :param header_list: A list of column header names from raw data.
+    :return: A list of indices that correspond to where the specific keyword shows up in the raw data column header list
+    """
+
+    index_list = list()
     for header in header_list:
         if header.startswith(keyword):
             index_list.append(header_list.index(header))
@@ -40,7 +62,15 @@ def index_range(keyword, header_list, dataframe):
     return index_list
 
 
-def get_missing_reversal_trials(df, column_names):
+def get_missing_reversal_trials(df):
+    """
+    This function fixes the missing trial numbers to the 1st/2nd reversal. If an animal does not reach the 1st reversal,
+    all the trials are counted towards the first reversal. If an animal does not reach the 2nd reversal and reaches the
+    1st reversal, all trials after the 1st reversal are counted towards the 2nd reversal. This is not reflected in the
+    ABET raw data!
+
+    :param df: The cleaned dataframe that has LD Train/LD Probe data.
+    """
     for index in df.iterrows():
         if np.isnan(index[1]['NumberOfTrialTo1stReversal']):
             # if not reach first reversal, make it the total number of trials
@@ -52,6 +82,14 @@ def get_missing_reversal_trials(df, column_names):
 
 
 def get_fixed_session_time(df, df2):
+    """
+    This function fixes the duration for each reversal, measured in seconds. The ABET raw data gives absolute time
+    instead of duration.
+
+    :param df: The cleaned dataframe that has LD Train/LD Probe data.
+    :param df2: A dataframe that represents the raw ABET data
+    """
+
     for index in df.iterrows():
         if index[1]['NumberOfReversal'] == 0:
             df.at[index[0], 'SessionLengthTo1stReversalDuration'] = df.at[index[0], 'SessionLength']
@@ -65,8 +103,18 @@ def get_fixed_session_time(df, df2):
                                                                                0], 'No trials to criterion - Condition (1)']
 
 
-# a function that gets the first reversal correctness
 def get_percent_correctness_first(df1, df2, column_names):
+    """
+    This function gets/fixes the percent correctness to the 1st reversal. If an animal does not reach the 1st reversal,
+    the overall percent correctness is the 1st reversal percent correctness. If an animal does not reach the
+    2nd reversal and reaches the 1st reversal, all trials after the 1st reversal are counted towards the 2nd reversal
+    percent correctness. This is not reflected in the ABET raw data!
+
+    :param df1: A dataframe that represents the raw ABET data file
+    :param df2: A dataframe that represents the cleaned LD Train/LD Probe data
+    :param column_names: A list of column names used to determine the percent correctness
+    """
+
     for index in df1.iterrows():
         stop_point = df1.at[index[0], 'No trials to criterion - Generic Evaluation (1)']
         # if did not reach first reversal, make the value the correct percentage value
@@ -84,8 +132,18 @@ def get_percent_correctness_first(df1, df2, column_names):
             df1.at[index[0], 'PercentCorrectTo1stReversal'] = df1[column_names[0:int_stop_point]].mean(axis=1)[index[0]]
 
 
-# function that gets the second reversal correctness
 def get_percent_correctness_second(df1, df2, column_names):
+    """
+    This function gets/fixes the percent correctness to the 2nd reversal. If an animal does not reach the 1st reversal,
+    the percent correctness for the 2nd reversal is NaN. If an animal does not reach the 2nd reversal and reaches the
+    1st reversal, all trials after the 1st reversal are counted towards the 2nd reversal percent correctness. This is
+    not reflected in the ABET raw data!
+
+    :param df1: A dataframe that represents the raw ABET data file
+    :param df2: A dataframe that represents the cleaned LD Train/LD Probe data
+    :param column_names: A list of column names used to determine the percent correctness
+    """
+
     for index in df1.iterrows():
         start_point = df1.at[index[0], 'No trials to criterion - Generic Evaluation (1)']
         stop_point = df2.at[index[0], 'NumberOfTrialTo2ndReversal'] + start_point
@@ -104,8 +162,19 @@ def get_percent_correctness_second(df1, df2, column_names):
                 df1[column_names[int_start_point:int_stop_point]].mean(axis=1)[index[0]]
 
 
-# function that gets which ld test was ran between (easy, hard, intermediate and undetermined)
 def get_test_type(df1, column_names):
+    """
+    This function determines the type of location discrimination test that the animal performed on. If the animal ran on
+    type = easy, then the squares will be far apart (7 and 12). If the animal ran on type = hard, then the squares will
+    be closer together (9 and 10). If the animal ran on type = intermediate, then the squares will be in the middle
+    (8 and 11). Otherwise, the type will be undetermined.
+
+    Note that these are all bottom row squares!
+
+    :param df1: A dataframe that represents the cleaned LD Train/LD Probe data
+    :param column_names: A list of column names used to determine the type
+    """
+
     some_list = df1[column_names].values.tolist()
     for index in df1.iterrows():
         if 8.0 in some_list[index[0]] or 11.0 in some_list[index[0]]:

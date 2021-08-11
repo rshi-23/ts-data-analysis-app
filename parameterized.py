ef52019dd0c99aa7d7da242828ba0ea135172c1b
raymon-shi <61591942+raymon-shi@users.noreply.github.com>
2021-08-10 23:04:52 -0400

    added docstrings to functions

--- parameterized.py
+++ parameterized.py
@@ -37,6 +37,17 @@ script_location = os.path.dirname(os.path.abspath(__file__))
 
 
 def block_parsing_ld_probe(df, test_type, total_blocks, parameters_list, writer):
+    """
+    This function is used for the LD Probe test. This function writes all the animal rows for all the blocks the current
+    parameter sheet.
+
+    :param df: A dataframe that represents the cleaned LD Probe data.
+    :param test_type: A string that represents the difficulty of the LD Probe test.
+    :param total_blocks: An integer that represents the maximum value of blocks within the dataframe
+    :param parameters_list: The list of parameters used to create each sheet of the Excel Workbook.
+    :param writer: Used to write and save the dataframe to the sheet on the Excel Workbook.
+    """
+
     for para in parameters_list:
         all_blocks = list()
         for block_number in range(1, int(total_blocks) + 1):
@@ -49,15 +60,23 @@ def block_parsing_ld_probe(df, test_type, total_blocks, parameters_list, writer)
 
 
 def ld_probe_parameterized(df):
+    """
+    This function is used for the LD Probe test. The function creates an Excel Workbook with each sheet being a testable
+    parameter from the LD Probe test. On each sheet, there will be the performance of each animal for all the blocks
+    based on difficulty (easy or hard). The Excel Workbook will be saved in the same location as the application.
+
+    :param df: A dataframe that represents cleaned LD Probe data.
+    """
+
     try:
         writer_easy = pd.ExcelWriter('All Blocks Parameters Easy.xlsx', engine='xlsxwriter')
         print('A file called All Blocks Parameters Easy.xlsx has been created at:', script_location)
         writer_hard = pd.ExcelWriter('All Blocks Parameters Hard.xlsx', engine='xlsxwriter')
         print('A file called All Blocks Parameters Hard.xlsx has been created at:', script_location)
     except PermissionError:
-        print(
-            'Either All Blocks Parameters Easy or All Blocks Parameters Hard is opened! Please close them!')
-        return
+        print('ld_probe_parameterized() error: All Blocks Parameters Easy/Hard.xlsx might be open! '
+              'Please close it before proceeding!')
+        return None
 
     max_blocks = df['Block'].max()
 
@@ -66,6 +85,17 @@ def ld_probe_parameterized(df):
 
 
 def day_parsing_ld_train(df, test_type, max_days, parameters_list, writer):
+    """
+    This function is used for the LD Train test. This function writes all the animal rows for all the days the animal
+    performed the test for the current parameter.
+
+    :param df: A dataframe that represents the cleaned LD Train data.
+    :param test_type: A string that represents the difficulty of the LD Probe test.
+    :param max_days: An integer that represents the maximum value of days within the dataframe
+    :param parameters_list: The list of parameters used to create each sheet of the Excel Workbook.
+    :param writer: Used to write and save the dataframe to the sheet on the Excel Workbook.
+    """
+
     for para in parameters_list:
         all_days = list()
         for day in range(1, int(max_days) + 1):
@@ -78,19 +108,39 @@ def day_parsing_ld_train(df, test_type, max_days, parameters_list, writer):
 
 
 def ld_train_parameterized(df):
+    """
+    This function is used for the LD Train test. The function creates an Excel Workbook with each sheet being a testable
+    parameter from the LD Train test. On each sheet, there will be the performance of each animal for all the days
+    the animal performed the test. The Excel Workbook will be saved in the same location as the application.
+
+    :param df: A dataframe that represents the cleaned LD Train data.
+
+    """
     try:
         writer_intermediate = pd.ExcelWriter('LD Train All Days Parameters.xlsx', engine='xlsxwriter')
         print('A file called LD Train All Days Parameters.xlsx has been created at:', script_location)
     except PermissionError:
         print(
             'LD Train All Days Parameters.xlsx is opened! Please close them!')
-        return
+        return None
 
     max_days = df['Day'].max()
     day_parsing_ld_train(df, 'intermediate', max_days, ld_parameters, writer_intermediate)
 
 
 def general_by_day_parsing(df, max_days, parameters_list, writer, test_name):
+    """
+    This function is a generic function for multiple test. This function writes all the animal rows for all the days
+    the animal performed the test for the current parameter.
+
+    :param df: A dataframe that represents the cleaned test data.
+    :param max_days: An integer that represents the maximum value of days within the dataframe
+    :param parameters_list: The list of parameters used to create each sheet of the Excel Workbook.
+    :param writer: Used to write and save the dataframe to the sheet on the Excel Workbook.
+    :param test_name: A string that represents the test name used for data setup.
+    :return:
+    """
+
     for para in parameters_list:
         all_days = list()
         for day in range(1, int(max_days) + 1):
@@ -103,6 +153,17 @@ def general_by_day_parsing(df, max_days, parameters_list, writer, test_name):
 
 
 def general_parameterized(df, name, parameter_list, test_name):
+    """
+    This function is used for the multiple test. The function creates an Excel Workbook with each sheet being a testable
+    parameter from multiple test. On each sheet, there will be the performance of each animal for all the days
+    the animal performed the test. The Excel Workbook will be saved in the same location as the application.
+
+    :param df: A dataframe that represents the cleaned test data.
+    :param name: A string that represents the name of the test the animal ran used for naming the file.
+    :param parameter_list: The list of parameters used to create each sheet of the Excel Workbook.
+    :param test_name: A string that represents the test name used for data setup.
+    :return:
+    """
     try:
         some_writer = pd.ExcelWriter(name + ' All Days Parameters.xlsx', engine='xlsxwriter')
     except PermissionError:
@@ -113,37 +174,45 @@ def general_parameterized(df, name, parameter_list, test_name):
 
 
 def general_para_button(data_setup_name, file_name, parameter_list):
+    """
+    This function creates the parameterized Excel Workbook for multiple test.
+
+    :param data_setup_name: A string that represents the name of the test used for data setup
+    :param file_name: A string that represents the the name of the test used to name the file.
+    :param parameter_list: A list that represents all the parameters that will be on each sheet of the workbook
+    """
+
     df = data_setup(data_setup_name)
     if df is not None:
         general_parameterized(df, file_name, parameter_list, data_setup_name)
         print('A file called', file_name, 'All Days Parameters.xlsx has been created at:', script_location)
 
 
-def criteria_widget_check(criteria):
-    if len(criteria.get()) != 0 and criteria.get().isnumeric():
-        criteria_value = int(criteria.get())
-    else:
-        print('Please enter a numeric criteria n days in a row!')
-        return
-
-    return criteria_value
-
-
 def pi_para_button(min_trials, percent_one, percent_two):
-    if len(min_trials.get()) != 0 and min_trials.get().isnumeric():
+    """
+    This function creates the parameterized Excel Workbook for the Punish Incorrect test.
+
+    :param min_trials: An entry widget that contains the value of the minimum required trials
+    :param percent_one: An entry widget that contains the value of the percent correctness for the first day
+    :param percent_two: An entry widget that contains the value of the percent correctness for the second day
+    """
+
+    try:
         minimum_trials = int(min_trials.get())
-    else:
-        print('Enter a numeric minimum req trials')
+    except ValueError:
+        print('pi_para_button() error: The minimum trials is empty or invalid!')
         return
-    if len(percent_one.get()) != 0 and percent_one.get().isnumeric():
+
+    try:
         correct_one = int(percent_one.get())
-    else:
-        print('Enter a numeric minimum first percent correct')
+    except ValueError:
+        print('pi_para_button() error: The percent correctness for the first day is empty or invalid!')
         return
-    if len(percent_two.get()) != 0 and percent_two.get().isnumeric():
+
+    try:
         correct_two = int(percent_two.get())
-    else:
-        print('Enter a numeric minimum second percent correct')
+    except ValueError:
+        print('pi_para_button() error: The percent correctness for the second day is empty or invalid!')
         return
 
     df = data_setup('PI')
@@ -153,19 +222,31 @@ def pi_para_button(min_trials, percent_one, percent_two):
 
 
 def acq_para_button(criteria, correct_amount, session_length):
-    criteria_value = criteria_widget_check(criteria)
+    """
+    This function creates the parameterized Excel Workbook for the Acquisition test.
 
-    if len(correct_amount.get()) != 0 and correct_amount.get().isnumeric():
+    :param criteria: An entry widget that contains the value of the n days in a row
+    :param correct_amount: An entry widget that contains the value of the correct trials amount
+    :param session_length: An entry widget that contains the value of the session length
+    """
+
+    try:
+        criteria_value = int(criteria.get())
+    except ValueError:
+        print('acq_para_button() error: The n days in a row criteria is empty or invalid!')
+        return None
+
+    try:
         correct_trials_num = int(correct_amount.get())
-    else:
-        print('Please enter a numeric minimum correct trials amount!')
-        return
+    except ValueError:
+        print('acq_para_button() error: The correct trials criteria is empty or invalid!')
+        return None
 
-    if len(session_length.get()) != 0 and session_length.get().isnumeric():
+    try:
         session_time_sec = int(session_length.get())
-    else:
-        print('Please enter a numeric minimum session length in seconds!')
-        return
+    except ValueError:
+        print('acq_para_button() error: The session length criteria is empty or invalid!')
+        return None
 
     df = data_setup('Acq')
     if df is not None:
@@ -174,49 +255,71 @@ def acq_para_button(criteria, correct_amount, session_length):
 
 
 def ext_para_button(criteria, omissions_amount):
-    if len(criteria.get()) != 0:
-        criteria_list = criteria.get().split('/')
-    else:
-        print('Please enter the criteria in the form of n/n+1 days')
+    """
+    This function creates the parameterized Excel Workbook for the Extinction test.
+
+
+    :param criteria: An entry value that contains the value of the n days and the n+1 days
+    :param omissions_amount: An entry value that contains the value of the omissions amount
+    """
+
+    criteria_list = criteria.get().split('/')
+
+    try:
+        criteria_value = int(criteria_list[0])
+        criteria_max_days = int(criteria_list[1])
+    except ValueError:
+        print('ext_para_button() error: The n/n+1 days criteria is empty or invalid!')
         return
 
-    if len(omissions_amount.get()) != 0 and omissions_amount.get().isnumeric():
+    try:
         omissions_count = int(omissions_amount.get())
-    else:
-        print('Please enter a numeric minimum omissions amount!')
+    except ValueError:
+        print('ext_para_button() error: The omissions amount is empty or invalid')
         return
 
     df = data_setup('Ext')
     if df is not None:
-        criteria_value = int(criteria_list[0])
-        criteria_max_days = int(criteria_list[1])
         get_extinction_all(df, criteria_value, criteria_max_days, omissions_count)
         general_parameterized(df, 'Extinction', ext_parameters, 'Ext')
 
 
 def ld_train_para_button(criteria, min_reversal_number):
-    if len(criteria.get()) != 0:
-        criteria_list = criteria.get().split('/')
-    else:
-        print('Please enter the criteria in the form of n/n+1 days')
-        return
+    """
+    This function creates the parameterized Excel Workbook for the LD Train test.
 
-    if len(min_reversal_number.get()) != 0 and min_reversal_number.get().isnumeric():
+    :param criteria: An entry widget that contains the value of the n days out of n+1 days.
+    :param min_reversal_number: An entry widget that contains the value of the minimum required reversal number
+    :return:
+    """
+
+    criteria_list = criteria.get().split('/')
+
+    try:
         min_rev = int(min_reversal_number.get())
-    else:
-        print('Please enter a numeric minimum reversal number!')
-        return
+    except ValueError:
+        print('ld_train_para_button() error: The minimum required reversal number is empty or invalid!')
+        return None
+
+    try:
+        criteria_value = int(criteria_list[0])
+        criteria_max_days = int(criteria_list[1])
+    except ValueError:
+        print('ld_train_para_button() error: The n/n+1 days criteria is empty or invalid!')
+        return None
 
     df = data_setup('LD Train')
     if df is not None:
         ld_train_delete_other_difficulties(df)
-        criteria_value = int(criteria_list[0])
-        criteria_max_days = int(criteria_list[1])
         get_ld_train_normal(df, criteria_value, criteria_max_days, min_rev)
         ld_train_parameterized(df)
 
 
 def ld_probe_para_button():
+    """
+    This function creates the parameterized Excel Workbook for the LD Probe test.
+    """
+
     df = data_setup('LD Probe')
     if df is not None:
         ld_probe_delete_other_difficulties(df)
@@ -225,6 +328,14 @@ def ld_probe_para_button():
 
 
 def make_parameterized_button(tk, root):
+    """
+    This function creates all the parameterized buttons found on the Parameterized sub-menu.
+
+    :param tk: The TKinter library
+    :param root: A specific frame where all the buttons will live on.
+    """
+
+    # creates general touchscreen buttons
     hab_one_para_btn = tk.Button(root, text='Habituation 1 Parameterized',
                                  command=lambda: general_para_button('Hab1', 'Habituation 1', hab_one_parameters),
                                  width=30)
@@ -246,9 +357,11 @@ def make_parameterized_button(tk, root):
                             width=30)
     mi_para_btn.grid(row=4, column=0)
 
+    # visual spacer between general ts and punish incorrect
     spacer_btw_ts_pi = tk.Label(root, text='', bg='#D6D6D6', width=57)
     spacer_btw_ts_pi.grid(row=5, columnspan=2)
 
+    # creates the punish incorrect criteria widgets
     pi_para_min_trials_labels = tk.Label(root, text='Enter the min req trial amount:')
     pi_para_min_trials_labels.grid(row=6, column=0)
     pi_para_min_trials_text = tk.Entry(root, width=30, justify='center')
@@ -264,14 +377,17 @@ def make_parameterized_button(tk, root):
     pi_para_correct_two_text = tk.Entry(root, width=30, justify='center')
     pi_para_correct_two_text.grid(row=8, column=1)
 
+    # creates the punish incorrect button
     pi_para_btn = tk.Button(root, text='Punish Incorrect Parameterized',
                             command=lambda: pi_para_button(pi_para_min_trials_text, pi_para_correct_one_text,
                                                            pi_para_correct_two_text), width=30)
     pi_para_btn.grid(row=9, column=0)
 
+    # visual spacer between punish incorrect and ld train
     spacer_btw_pi_ld = tk.Label(root, text='', bg='#D6D6D6', width=57)
     spacer_btw_pi_ld.grid(row=10, columnspan=2)
 
+    # creates the ld train criteria widgets
     ld_train_criteria_label = tk.Label(root, text='Enter criteria as n days/n+1 days: ')
     ld_train_criteria_label.grid(row=11, column=0)
     ld_train_criteria_text = tk.Entry(root, width=30, justify='center')
@@ -282,6 +398,7 @@ def make_parameterized_button(tk, root):
     ld_train_min_rev_num_text = tk.Entry(root, width=30, justify='center')
     ld_train_min_rev_num_text.grid(row=12, column=1)
 
+    # creates the ld buttons
     ld_train_para_btn = tk.Button(root, text='LD Train Parameterized',
                                   command=lambda: ld_train_para_button(ld_train_criteria_text,
                                                                        ld_train_min_rev_num_text), width=30)
@@ -290,9 +407,11 @@ def make_parameterized_button(tk, root):
     ld_probe_para_btn = tk.Button(root, text='LD Probe Parameterized', command=ld_probe_para_button, width=30)
     ld_probe_para_btn.grid(row=14, column=0)
 
+    # visual spacer between ld and acquisition
     spacer_btw_ld_acq = tk.Label(root, text='', bg='#D6D6D6', width=57)
     spacer_btw_ld_acq.grid(row=15, columnspan=2)
 
+    # creates the acquisition criteria widgets
     acq_para_label = tk.Label(root, text='Enter criteria as n days in a row: ')
     acq_para_label.grid(row=16, column=0)
     acq_para_text = tk.Entry(root, width=30, justify='center')
@@ -308,18 +427,21 @@ def make_parameterized_button(tk, root):
     acq_para_session_len_text = tk.Entry(root, width=30, justify='center')
     acq_para_session_len_text.grid(row=18, column=1)
 
+    # creates the acquisition button
     acq_para_btn = tk.Button(root, text='Acquisition Parameterized',
                              command=lambda: acq_para_button(acq_para_text, acq_para_corrects_text,
                                                              acq_para_session_len_text),
                              width=30)
     acq_para_btn.grid(row=19, column=0)
 
+    # visual spacer between acquisition and extinction
     spacer_btw_acq_ext = tk.Label(root, text='', bg='#D6D6D6', width=57)
     spacer_btw_acq_ext.grid(row=20, columnspan=2)
 
+    # creates extinction criteria widgets
     ext_para_label = tk.Label(root, text='Enter criteria as n days/n+1 days:')
     ext_para_label.grid(row=21, column=0)
-    ext_para_text = tk.Entry(root, width=30)
+    ext_para_text = tk.Entry(root, width=30, justify='center')
     ext_para_text.grid(row=21, column=1)
 
     ext_para_omissions_label = tk.Label(root, text='Enter the min omissions req:')
@@ -327,6 +449,7 @@ def make_parameterized_button(tk, root):
     ext_para_omissions_text = tk.Entry(root, width=30, justify='center')
     ext_para_omissions_text.grid(row=22, column=1)
 
+    # creates extinction buttons
     ext_para_btn = tk.Button(root, text='Extinction Parameterized',
                              command=lambda: ext_para_button(ext_para_text, ext_para_omissions_text),
                              width=30)
--- ts_main.py
+++ ts_main.py
@@ -7,21 +7,24 @@ root.geometry('404x765')
 root.title('Raymon Shi Touchscreen Data Analysis App')
 root.rowconfigure(0, weight=1)
 root.columnconfigure(0, weight=1)
-root.resizable(False, False)
 
 
 def display_frame(frame_page):
+    """
+    This function simply raises a selected frame to the top.
+
+    :param frame_page: The frame that will be pushed to the top of the window.
+    """
+
     frame_page.tkraise()
 
 
 main_page_frame = tk.Frame(root)
 main_page_frame.grid(row=0, column=0, sticky='nsew')
-
 main_page_frame.columnconfigure(0, weight=1)
 
 general_ts_frame = tk.Frame(root)
 general_ts_frame.grid(row=0, column=0, sticky='nsew')
-general_ts_frame.columnconfigure(0, weight=1)
 
 ld_train_frame = tk.Frame(root)
 ld_train_frame.grid(row=0, column=0, sticky='nsew')
@@ -37,6 +40,10 @@ parameterized_frame.grid(row=0, column=0, sticky='nsew')
 
 
 def main_page_frame_buttons():
+    """
+    This function creates the buttons that appear on the main menu.
+    """
+
     main_general_ts_btn = tk.Button(main_page_frame, text='General Touchscreen',
                                     command=lambda: display_frame(general_ts_frame))
     main_general_ts_btn.grid(row=1, column=0)
@@ -53,11 +60,17 @@ def main_page_frame_buttons():
                                        command=lambda: display_frame(parameterized_frame))
     main_parameterized_btn.grid(row=5, column=0)
     main_github_code_btn = tk.Button(main_page_frame, text='Github Code Page', command=lambda: webbrowser.open(
-        'https://github.com/raymon-shi/ts-data-analysis-script'))
+        'https://github.com/raymon-shi/ts-data-analysis-app'))
     main_github_code_btn.grid(row=6, column=0)
 
 
 def main_menu_buttons(frame):
+    """
+    This function adds a button that leads back to the main menu on every frame.
+
+    :param frame: The page that the main menu button will be added on
+    """
+
     main_menu_btn = tk.Button(frame, text='Main Menu', command=lambda: display_frame(main_page_frame), width=30)
     spacer_btn = tk.Label(frame, text='', width=57, bg='#D6D6D6')
     spacer_btn.grid(row=30, columnspan=2)
@@ -65,6 +78,10 @@ def main_menu_buttons(frame):
 
 
 def make_gui():
+    """
+    This function creates all the buttons and main graphic user interface for the application.
+    """
+
     make_general_ts_buttons(tk, general_ts_frame)
     main_menu_buttons(general_ts_frame)
 
